9cd7518cd3ea4b448e7f296533bda28d8a4c4b7a
Alexander Topchiy <topchiyau@gmail.com>
2019-06-21 22:20:54 +0300

    Added main UI things

--- app/src/main/java/com/wyverx/goals/goals/GoalsActivity.java
+++ app/src/main/java/com/wyverx/goals/goals/GoalsActivity.java
@@ -1,13 +1,15 @@
-package com.wyverx.goals;
+package com.wyverx.goals.goals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.wyverx.goals.R;
+
+public class GoalsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_goals);
     }
 }
---
+++ app/src/main/java/com/wyverx/goals/goals/GoalsFragment.java
@@ -0,0 +1,6 @@
+package com.wyverx.goals.goals;
+
+import android.support.v4.app.Fragment;
+
+public class GoalsFragment extends Fragment {
+}
